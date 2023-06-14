dbb8c721dd8e2903199c9eb5ab85e8fffeda4bea
Albert Bertuldo <johnalbert.bertuldo@gcash.com>
2023-06-14 13:13:31 +0800

    Added test folder for testing

---
+++ src/test/java/group3/FlightAppTest.java
@@ -0,0 +1,96 @@
+package group3;
+
+import group3.accounts.Account;
+import group3.constants.AccountType;
+import group3.constants.Address;
+import group3.constants.Const;
+import group3.constants.SeatClass;
+import group3.flight.Airplane;
+import group3.flight.Airport;
+import group3.flight.Flight;
+import group3.service.FlightService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FlightAppTest {
+    private FlightService fs;
+    private Account tempAcc;
+    private FlightService flightService;
+    private Account testAccount;
+    private Flight testFlight;
+
+    @BeforeEach
+    public void setUp() {
+        fs = new FlightService();
+        tempAcc = new Account("id", "password");
+        Airport departure = new Airport("DEP", new Address("Departure City", "Departure State",3000,"Philippines"));
+        Airport arrival = new Airport("ARR", new Address("Arrival City", "Arrival State",3000,"Philippines"));
+        Airplane airplane = new Airplane("ABC123", "2021","Captain Albert",2023);
+        LocalDate flightDate = LocalDate.now();
+        testFlight = new Flight("FL123", departure, arrival, airplane, flightDate);
+        testAccount = new Account("John Doe", "john@example.com", AccountType.PASSENGER);
+    }
+
+    @Test
+    public void testRegisterUser() {
+        fs.registerUser("user", Const.ADDR1, "email", "phone", tempAcc);
+        assertEquals(1, fs.numberOfAccounts());
+    }
+
+    @Test
+    public void testRegisterUserDuplicateAccount() {
+        fs.registerUser("user", Const.ADDR1, "email", "phone", tempAcc);
+        fs.registerUser("user", Const.ADDR1, "email", "phone", tempAcc);
+        assertEquals(1, fs.numberOfAccounts());
+    }
+
+    @Test
+    public void testNumberOfRegisteredUsers() {
+        fs.registerUser("user", Const.ADDR1, "email", "phone", tempAcc);
+        assertEquals(1, fs.numberOfRegisteredUsers());
+    }
+
+    @Test
+    public void testAddFlight() {
+        fs.addFlight(Const.FL1);
+        fs.addFlight(Const.FL2);
+        fs.addFlight(Const.FL3);
+        assertEquals(3, fs.getNumberOfFlights());
+    }
+
+
+    @Test
+    public void testBookFlight() {
+        fs.addFlight(testFlight);
+        //fs.registerUser("John Doe", new Address(), "john@example.com", "1234567890", testAccount);
+        fs.bookFlight("FL123", 5, SeatClass.ECONOMY, testAccount);
+        int bookedSeats = testFlight.getPlane().getNumberOfFilledSeats(SeatClass.ECONOMY);
+        assertEquals(5, bookedSeats);
+    }
+
+
+    @Test
+    public void testFindFlightByNumber() {
+        fs.addFlight(testFlight);
+        Flight foundFlight = fs.findFlightByNumber("FL123");
+
+        Assertions.assertNotNull(foundFlight);
+        assertEquals(testFlight, foundFlight);
+    }
+
+
+    @Test
+    public void testDisplayTransactionHistory() {
+        fs.addFlight(Const.FL1);
+        fs.bookFlight(Const.FL1.getFlightNumber(), 5, SeatClass.ECONOMY, tempAcc);
+        fs.displayTransactionHistory();
+    }
+
+    //Add more tests here
+}
